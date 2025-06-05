879cbdf7647dd8b92c0fac84b14f568826f1719c
pesho <peshooomanov@gmail.com>
2025-06-05 17:26:43 +0300

    server: GET /items/:id endpoint

--- server/src/controllers/item.controller.ts
+++ server/src/controllers/item.controller.ts
@@ -25,4 +25,16 @@ const addItem = (req: express.Request, res: express.Response) => {
   res.status(201).json(newItem);
 };
 
-export default { getItems, addItem };
+const getItemById = (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  const item = items.find((item) => item.id === id);
+
+  if (!item) {
+    res.status(404).json({ error: "Item not found." });
+    return;
+  }
+
+  res.status(200).json(item);
+};
+
+export default { getItems, addItem, getItemById };
--- server/src/routes/item.route.ts
+++ server/src/routes/item.route.ts
@@ -3,7 +3,8 @@ import itemController from "../controllers/item.controller";
 
 const router: Router = express.Router();
 
-router.get("/getItems", itemController.getItems);
-router.post("/addItem", itemController.addItem);
+router.get("/items", itemController.getItems);
+router.post("/items", itemController.addItem);
+router.get("/items/:id", itemController.getItemById);
 
 export default router;
